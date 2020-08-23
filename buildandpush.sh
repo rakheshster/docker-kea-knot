b916244b4fe50e405ad4e4b70864578fa35566e5
Rakhesh Sasidharan <me@rakhesh.com>
2020-08-23 16:12:11 +0100

    Added License

--- buildandpush.sh
+++ buildandpush.sh
@@ -8,4 +8,5 @@ fi
 
 VERSION=v0.1.0
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t ${IMAGE}:${VERSION}
+docker buildx build --platform linux/amd64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t ${IMAGE}:${VERSION}
+# NOTE: no linux/arm64. The build was breaking for that. 
