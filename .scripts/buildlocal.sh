d98924419e15a35c3893bb8844b633465916d7ce
Rakhesh Sasidharan <me@rakhesh.com>
2021-01-24 18:35:42 +0000

    small changes to my scripts. nothing significant.

--- .scripts/buildlocal.sh
+++ .scripts/buildlocal.sh
@@ -9,10 +9,4 @@ if ! command -v jq &> /dev/null; then echo "Cannot find jq. Exiting ..."; exit 1
 VERSION=$(jq -r '.version' $BUILDINFO)
 IMAGENAME=$(jq -r '.imagename' $BUILDINFO)
 
-# delete an existing image of the same name if it exists
-# thanks to https://stackoverflow.com/questions/30543409/how-to-check-if-a-docker-image-with-a-specific-tag-exist-locally
-if [[ $(docker image inspect ${IMAGENAME} 2>/dev/null) == "" ]]; then
-    docker rmi -f ${IMAGENAME}:${VERSION}
-fi
-
-docker build -t ${IMAGENAME}:${VERSION} -t ${IMAGENAME}:latest .
+docker build -t ${IMAGENAME}:${VERSION} -t ${IMAGENAME}:latest $(pwd)
--- .scripts/buildxandpush.sh
+++ .scripts/buildxandpush.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Usage ./buildxandpush.sh
 
+# This variant makes use of the buildx plugin and can do multi-arch builds. It also pushes the DockerHub.
+# Not all platforms have all archs if I am building locally. For example M1 Macs don't do linux/386 anymore. 
+# ARCH="linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6"
+ARCH="linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6"
+
 BUILDINFO="$(pwd)/buildinfo.json"
 if ! [[ -r "$BUILDINFO" ]]; then echo "Cannot find $BUILDINFO file. Exiting ..."; exit 1; fi
 
@@ -9,4 +14,4 @@ if ! command -v jq &> /dev/null; then echo "Cannot find jq. Exiting ..."; exit 1
 VERSION=$(jq -r '.version' $BUILDINFO)
 IMAGENAME=$(jq -r '.imagename' $BUILDINFO)
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t ${IMAGENAME}:${VERSION} -t ${IMAGENAME}:latest --progress=plain
+docker buildx build --platform $ARCH -t ${IMAGENAME}:${VERSION} -t ${IMAGENAME}:latest --progress=plain --push $(pwd)
